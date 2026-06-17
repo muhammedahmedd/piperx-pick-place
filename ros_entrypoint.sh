9c0e906ff1940d573f9acac1f92c18218f36fb53
muhammedahmedd <mohamed.ahmed.1@vanderbilt.edu>
2026-06-17 14:10:21 -0500

    Add Docker setup for ros2 humble workspace

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 source /opt/ros/humble/setup.bash
 
-if [ -f /workspace/piperx_ws/install/setup.bash ]; then
-    source /workspace/piperx_ws/install/setup.bash
+if [ -f /workspace/piperx_sim_ws/install/setup.bash ]; then
+    source /workspace/piperx_sim_ws/install/setup.bash
 fi
 
 exec "$@"
